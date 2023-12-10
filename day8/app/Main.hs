e82bcdfab6eb5c360f7d6dfea988706c58dadce3
Raphaël Colin <raphael.colin68@gmail.com>
2023-12-10 19:08:00 +0100

    WIP: some progress on day 8

--- day8/app/Main.hs
+++ day8/app/Main.hs
@@ -4,11 +4,24 @@ import qualified Data.Map as Map
 import Prelude hiding(Left, Right)
 import Data.Char
 
-data Instruction = Left | Right deriving(Show)
+data Instruction = Left | Right deriving(Show, Eq)
+type InstructionStream = ([Instruction], Int)
 type Node = String
 type NodePath = (Node, (Node, Node))
 type Network = Map.Map Node (Node, Node)
 type Puzzle = ([Instruction], Network)
+
+getCurInstr :: InstructionStream -> Instruction
+getCurInstr (instr, id) = instr !! id
+
+getCurInstrId :: InstructionStream -> Int
+getCurInstrId (_, id) = id
+
+advanceStream :: InstructionStream -> InstructionStream
+advanceStream (instr, id) = (instr, (id + 1) `mod` length instr)
+
+fromInstrList :: [Instruction] -> InstructionStream
+fromInstrList instr = (instr, 0)
 
 parseInstruction :: Char -> Instruction
 parseInstruction 'L' = Left
@@ -40,35 +53,30 @@ followInstruction :: Instruction -> (Node, Node) -> Node
 followInstruction Left (n , _) = n
 followInstruction Right (_, n) = n
 
-advance :: Network -> [Node] -> Instruction -> [Node]
-advance net n instr =
-  let nodes = map (net Map.!) n
-   in map (followInstruction instr) nodes
+advance :: Network -> Node -> Instruction -> Node
+advance net n instr = followInstruction instr (net Map.! n)
 
-getPath :: Network -> [Node] -> (Node -> Bool) -> [Instruction] -> [[Node]]
+getPath :: Network -> Node -> (Node -> Bool) -> [Instruction] -> [Node]
 getPath net cur isEnd (i:is) 
-  | all isEnd cur = []
+  | isEnd cur = []
   | otherwise = cur:getPath net (advance net cur i) isEnd is
 getPath _ _ _ [] = []
 
-getPathUntilCycle :: Network -> Node -> [Node] -> Node -> [Instruction] -> ([Node], [Instruction])
-getPathUntilCycle net cur path lastEndNode (i:is)
-  | cur == lastEndNode = (path, is)
-  | (last cur) == 'Z' = getPathUntilCycle net next (cur:path) cur is 
-  | otherwise = getPathUntilCycle net next (cur:path) lastEndNode is
-  where next = head $ advance net [cur] i
-getPathUntilCycle _ _ _ _ [] = ([], [])
+getPathUntilCycle :: Network -> Node -> [(Node, Int)] -> InstructionStream -> [(Node, Int)]
+getPathUntilCycle net cur path instS
+  | (cur, getCurInstrId instS) `elem` path = path
+  | otherwise = getPathUntilCycle net next ((cur, getCurInstrId instS):path) (advanceStream instS)
+  where next = advance net cur (getCurInstr instS)
 
-getCycle :: Network -> Node -> Node -> [Instruction] -> [Node]
-getCycle net cur start (i:is)
-  | next == start = [cur]
-  | otherwise = cur:getCycle net next start is
-  where next = head $ advance net [cur] i
-getCycle _ _ _ [] = []
+getCycle :: Network -> Node -> (Node, Int) -> InstructionStream -> [Node]
+getCycle net cur start instrS
+  | (next, getCurInstrId $ advanceStream instrS) == start = [cur]
+  | otherwise = cur:getCycle net next start (advanceStream instrS)
+  where next = advance net cur (getCurInstr instrS)
 
 solution1 :: Puzzle -> Int
 solution1 (instrs, net) =
-  length $ getPath net ["AAA"] isEnd (cycle instrs)
+  length $ getPath net "AAA" isEnd (cycle instrs)
     where isEnd = all (== 'Z')
 
 stepsUntilCyclesMeet :: [Int] -> [Int] -> [Int] -> Int
@@ -77,30 +85,26 @@ stepsUntilCyclesMeet startPos endPos cyclelengths
   | otherwise = 1 + stepsUntilCyclesMeet [(a + 1) `mod` b | (a, b) <- zip startPos cyclelengths] endPos cyclelengths
   where step = maximum cyclelengths - minimum startPos
 
-solution2 :: Puzzle -> ([[Node]], [[Node]])
+solution2 :: Puzzle -> [[(Node, Int)]]
 solution2 (instrs, net) =
   let startNodes = filter ((== 'A') . last) $ Map.keys net
-      pathsRemInstr = map (\n -> getPathUntilCycle net n [] "ZZZZZZ" (cycle instrs)) startNodes
-      cycles = [getCycle net (head paths) (head paths) i | (paths, i) <- pathsRemInstr]
-  in (map (drop 1) $ fst (unzip pathsRemInstr), cycles)
+      stream = fromInstrList instrs
+   in map (\n -> getPathUntilCycle net n [] stream) startNodes
 
 -- We need to to compute the product of the cycle lengths minus the time it takes to get to the initial start from the 0 point
 main :: IO ()
 main = do
-  inputStr <- readFile "example2.txt"
+  inputStr <- readFile "input.txt"
   let input = lines inputStr 
       parsedInput@(instr, net) = parseInput input
-      (paths, cycles) = solution2 parsedInput
-      max = maximum $ map length paths
-      start = [((-a) + max) `mod` b | (a, b) <- zip (map length paths) (map length cycles)]
-      cyclelengths = map length $ map (reverse . reverse . (dropWhile ((/= 'Z') . last))) $ cycles
-  putStrLn $ show parsedInput
-  putStrLn $ show $ filter ((== 'A') . last) $ Map.keys (snd parsedInput)
+      paths = solution2 parsedInput
+      lasts = map head paths
+      cycles = map (\n -> getCycle net (fst n) n (instr, snd n)) lasts
+--      cyclelengths = map length $ map (reverse . reverse . (dropWhile ((/= 'Z') . last))) $ cycles
+--  putStrLn $ show parsedInput
 --  putStrLn $ "Solution to problem 1: " ++ (show $ solution1 parsedInput)
 --  putStrLn $ "Solution to problem 2: " ++ (show $ solution2 parsedInput)
-  putStrLn $ show $  solution2 parsedInput
-  putStrLn $ show $ cyclelengths
+--  putStrLn $ show $ solution2 parsedInput
   putStrLn $ show $ map length paths
-  putStrLn $ show $ start
-  putStrLn $ show $ max
-  putStrLn $ show $ stepsUntilCyclesMeet (replicate (length start) 0) start cyclelengths
+  putStrLn $ show $ map length cycles
+  putStrLn $ show $ map (filter ((=='Z') . last)) cycles
